6b8ca495692dadf5525bc36ac4cbb6ebabc0d141
Eugene Goryachev <87129641+saladware@users.noreply.github.com>
2023-04-08 13:18:54 +0300

    Update context_base.py

--- examples/context_base.py
+++ examples/context_base.py
@@ -4,7 +4,7 @@ from achat import SonicChat
 
 
 async def main():
-    async with SonicChat(token="d93c8f31-bad7-4bfd-85e0-e700e3c7d976") as chat:
+    async with SonicChat(token="your api token here") as chat:
         response = await chat.ask("Who is Elon Musk?")
         print(response.message)
 
